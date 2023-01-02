8e2ba2738b0aba6ffee9ddb6174acbc50681dd40
Prakhar Pratap Singh <prakharpratap97@gmail.com>
2023-01-02 10:22:24 +0530

    tabs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import Tabs from "./components/Tabs";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <h1>Tabs Demo</h1>
+    <Tabs>
+      <div label="Slider">
+         <em>Hey</em>!
+      </div>
+      <div label="Video">
+         <em>Hello</em>!
+      </div>
+      <div label="Comparison">
+         <em>Hi</em>!
+      </div>
+    </Tabs>
+  </div>
   );
 }
 
